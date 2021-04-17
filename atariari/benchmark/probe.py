541749b6b685311bfec67ec361321718198e39a8
Mina Khan <minakhan01@gmail.com>
2021-04-17 08:28:47 -0400

    Update probe.py

--- atariari/benchmark/probe.py
+++ atariari/benchmark/probe.py
@@ -241,11 +241,19 @@ class ProbeTrainer():
 
     def log_results(self, epoch_idx, *dictionaries):
         print("Epoch: {}".format(epoch_idx))
+        if wandb is not None:
+            self.log_wandb_results(epoch_idx, *dictionaries)
         for dictionary in dictionaries:
             for k, v in dictionary.items():
                 print("\t {}: {:8.4f}".format(k, v))
             print("\t --")
-
+            
+    def log_wandb_results(self, epoch_idx, *dictionaries):
+        print("Epoch: {}".format(epoch_idx))
+        for dictionary in dictionaries:
+            for k, v in dictionary.items():
+                wandb.log("\t {}: {:8.4f}".format(k, v))
+            wandb.log("\t --")    
 
 def postprocess_raw_metrics(acc_dict, f1_dict):
     acc_overall_avg, f1_overall_avg = compute_dict_average(acc_dict), \
