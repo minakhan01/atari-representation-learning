b098611aedeb34b2bdf916765d6036cb3e4db166
Mina Khan <minakhan01@gmail.com>
2021-04-18 10:04:35 -0400

    Update encoders.py

--- atariari/methods/encoders.py
+++ atariari/methods/encoders.py
@@ -6,6 +6,9 @@ import time
 from atariari.benchmark.utils import download_run
 from atariari.benchmark.episodes import checkpointed_steps_full_sorted
 import os
+import clip
+from PIL import Image
+from torchvision.transforms import Compose, Resize, Normalize
 
 class Flatten(nn.Module):
     def forward(self, x):
@@ -160,7 +163,32 @@ class NatureCNN(nn.Module):
             }
         return out
 
+class ClipEncoder(nn.Module):
+  def __init__(self, input_channels, feature_size):
+    super().__init__()
+    self.device = "cuda" if torch.cuda.is_available() else "cpu"
+    self.clip_model, _ = clip.load("ViT-B/32", device=self.device)
+    self.preprocess = Compose([
+        Resize((224, 224), interpolation=Image.BICUBIC),
+        Normalize(
+          (0.48145466, 0.4578275, 0.40821073),
+          (0.26862954, 0.26130258, 0.27577711)
+        )
+    ])
+    self.feature_size = feature_size
+    self.input_channels = input_channels
 
+  def forward(self, inputs):
+      x = self.get_clip_features(inputs)
+      x = x.view(x.size(0), -1)
+      return x
+  
+  def get_clip_features(self, image):
+    with torch.no_grad():
+      image_features = self.clip_model.encode_image(self.preprocess(image)).float()
+    return image_features
+
+my_encoder = ClipEncoder(input_channels=3,feature_size=512)
 
 class PPOEncoder(nn.Module):
     def __init__(self, env_name, checkpoint_index):
